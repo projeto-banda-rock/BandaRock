69e503148fa1afd1c0ae2e7045efbf037948e3ba
Murillo Henrique <murillo.h.silva6@aluno.senai.br>
2026-08-05 13:10:35 -0300

    todo o front da pagina dos Beatles

---
+++ script/scriptBeatles.js
@@ -0,0 +1,4 @@
+function abrirModal(id) { document.getElementById(id).style.display = 'flex'; }
+function fecharModal(id) { document.getElementById(id).style.display = 'none'; }
+function abrirModal2() { document.getElementById('modal2').classList.add('active'); }
+function fecharModal2() { document.getElementById('modal2').classList.remove('active'); }
