d91438f847fcbf73b5e91f948bd6bc877ab2c8e1
Lais Zamboni <dev.lais.zamboni@gmail.com>
2026-08-05 14:12:38 -0300

    Modais da página Led Zeppelin completos

---
+++ script/ledzeppelin.js
@@ -0,0 +1,4 @@
+function openFullscreen() { document.getElementById('fsModal').style.display = 'flex'; }
+function closeFullscreen() { document.getElementById('fsModal').style.display = 'none'; }
+function openLeftDrawer() { document.getElementById('leftDrawer').classList.add('active'); }
+function closeLeftDrawer() { document.getElementById('leftDrawer').classList.remove('active'); }
