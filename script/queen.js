0a95edee7a144d088c5bb15074de31ebbcc57e96
Lais Zamboni <dev.lais.zamboni@gmail.com>
2026-08-05 15:18:46 -0300

    Modal da página queen realizado

---
+++ script/queen.js
@@ -0,0 +1,4 @@
+function openCrown() { document.getElementById('crownModal').style.display = 'flex'; }
+function closeCrown() { document.getElementById('crownModal').style.display = 'none'; }
+function openPanel() { document.getElementById('panelSide').classList.add('active'); }
+function closePanel() { document.getElementById('panelSide').classList.remove('active'); }
