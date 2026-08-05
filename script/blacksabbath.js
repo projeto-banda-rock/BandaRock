3c2d735c789cb0c5c2fef478d6836d64f1b0d1f0
Lais Zamboni <dev.lais.zamboni@gmail.com>
2026-08-05 15:51:28 -0300

    Modal de Black Sabbath finalizado

---
+++ script/blacksabbath.js
@@ -0,0 +1,4 @@
+function openGothic() { document.getElementById('gothicModal').style.display = 'flex'; }
+function closeGothic() { document.getElementById('gothicModal').style.display = 'none'; }
+function openBottomGothic() { document.getElementById('bottomGothic').classList.add('active'); }
+function closeBottomGothic() { document.getElementById('bottomGothic').classList.remove('active'); }
