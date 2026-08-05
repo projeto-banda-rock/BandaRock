c1e379738a8bb3e58700223b65f373213e84d63f
Lais Zamboni <dev.lais.zamboni@gmail.com>
2026-08-05 12:54:53 -0300

    Página The Rolling Stones finalizada

---
+++ script/stones.js
@@ -0,0 +1,3 @@
+function toggleDrawer() { document.getElementById('drawer1').classList.toggle('open'); }
+function openLeatherModal() { document.getElementById('leatherModal').style.display = 'flex'; }
+function closeLeatherModal() { document.getElementById('leatherModal').style.display = 'none'; }
