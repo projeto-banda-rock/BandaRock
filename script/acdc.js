a432821b95226ac793af9d2d03aa0c8699f9e619
Murillo Henrique <murillo.h.silva6@aluno.senai.br>
2026-08-05 15:13:02 -0300

    adicionado modal a acdc

---
+++ script/acdc.js
@@ -0,0 +1,4 @@
+function openCorner() { document.getElementById('cornerModal').classList.add('open'); }
+function closeCorner() { document.getElementById('cornerModal').classList.remove('open'); }
+function openShock() { document.getElementById('shockModal').style.display = 'flex'; }
+function closeShock() { document.getElementById('shockModal').style.display = 'none'; }
