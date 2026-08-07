c67910923119c6e32f5a0e60c5b0e86c31ac1101
Lais Zamboni <dev.lais.zamboni@gmail.com>
2026-08-07 07:32:43 -0300

    JS concluído

---
+++ script/metallica.js
@@ -0,0 +1,4 @@
+function openMetal() { document.getElementById('metalModal').style.display = 'flex'; }
+function closeMetal() { document.getElementById('metalModal').style.display = 'none'; }
+function openThrash() { document.getElementById('thrashFS').style.display = 'flex'; }
+function closeThrash() { document.getElementById('thrashFS').style.display = 'none'; }
